c266087f2ed8d627d2a9e6c0ea19f536c9f9d054
Peter Epsteen <petereps1@gmail.com>
2018-01-13 12:14:59 -0800

    First Commit

---
+++ CPASite/js/script.js
@@ -0,0 +1,38 @@
+
+$( window ).on( "load", function() {
+var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["2016", "2015", "2014"],
+            datasets: [{
+                label: 'Ratio',
+                data: [5, 2, 3],
+                backgroundColor: [
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
+
+
+
+ });
