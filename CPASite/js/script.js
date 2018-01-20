a9397a3762a0dd1249006c3e511abc60059a2b5b
peter epsteen <peterepsteen@Jennifers-iMac.home>
2018-01-20 14:57:13 -0800

    UI pretty much done

--- CPASite/js/script.js
+++ CPASite/js/script.js
@@ -1,6 +1,5 @@
-
-$( window ).on( "load", function() {
-var ctx = document.getElementById("myChart").getContext('2d');
+$(window).on("load", function() {
+    var ctx = document.getElementById("myChart").getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
@@ -33,17 +32,17 @@ var ctx = document.getElementById("myChart").getContext('2d');
             scales: {
                 yAxes: [{
                     ticks: {
-                        beginAtZero:true,
+                        beginAtZero: true,
                         fontColor: '#EEEEEE'
                     },
                     gridLines: {
-                       color: '#EEEEEE'
+                        color: '#EEEEEE'
                     },
                     scaleLabel: {
                         fontColor: '#EEEEEE'
                     }
                 }],
-                xAxes : [{
+                xAxes: [{
                     ticks: {
                         fontColor: '#EEEEEE'
                     },
@@ -59,4 +58,78 @@ var ctx = document.getElementById("myChart").getContext('2d');
 
 
 
- });
+});
+var balanceTab = $('#balance-sheet-tab');
+var incomeTab = $('#income-statement-tab')
+balanceTab.click(function() {
+    if (!balanceTab.hasClass('active')) {
+        balanceTab.toggleClass('active');
+        incomeTab.toggleClass('active');
+    }
+});
+incomeTab.click(function() {
+    if (!incomeTab.hasClass('active')) {
+        incomeTab.toggleClass('active');
+        balanceTab.toggleClass('active');
+    }
+});
+
+function updateCustomDescription() {
+    $('.customRatioTop').text(getOptionsString('sbNum'));
+    $('.customRatioBottom').text(getOptionsString('sbDem'));
+}
+
+function getOptionsString(selectID) {
+    var options = $('#' + selectID + ' option').clone();
+    var optionsString = '';
+    $.each(options, function(ele) {
+        optionsString += options[ele].innerHTML;
+    });
+    return optionsString;
+} 
+
+$('#numPlus').on('click', function() {
+    var options = $('#sbOne option:selected').clone();
+    var optionsString = '';
+    $.each(options, function(ele) {
+        options[ele].innerHTML = ' + ' + options[ele].innerHTML;
+        optionsString += options[ele].innerHTML;
+
+    });
+    $('#sbNum').append(options);
+    // $('.customRatioTop').text(getOptionsString('sbNum'));
+    updateCustomDescription();
+});
+$('#numMinus').on('click', function() {
+    var options = $('#sbOne option:selected').clone();
+    $.each(options, function(ele) {
+        options[ele].innerHTML = ' - ' + options[ele].innerHTML;
+    });
+    $('#sbNum').append(options);
+    updateCustomDescription();
+});
+$('#denominatorPlus').on('click', function() {
+    var options = $('#sbOne option:selected').clone();
+    $.each(options, function(ele) {
+        options[ele].innerHTML = ' + ' + options[ele].innerHTML;
+    });
+    $('#sbDem').append(options);
+    updateCustomDescription();
+});
+$('#denominatorMinus').on('click', function() {
+    var options = $('#sbOne option:selected').clone();
+    $.each(options, function(ele) {
+        options[ele].innerHTML = ' - ' + options[ele].innerHTML;
+    });
+    $('#sbDem').append(options);
+    updateCustomDescription();
+});
+$('#remove').on('click', function() {
+    $('select.newRatioSelect option:selected').remove();
+    updateCustomDescription();
+});
+
+$('#removeAll').on('click', function() {
+    $('select.newRatioSelect').empty();
+    updateCustomDescription();
+});
